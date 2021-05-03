6c9568dbcf6c9119dac93d6ecd6a4d7d7c5403a2
Zowie Erickson <zowie.erickson@gmail.com>
2021-05-03 11:03:14 -0400

    added seach bar, tweaked layout

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ const gridContainer = document.querySelector(".grid-container");
 const overlay = document.querySelector(".overlay");
 const modalContainer = document.querySelector(".modal-content");
 const modalClose = document.querySelector(".modal-close");
+const searchBar = document.querySelector("#searchBar");
 
 // -------------------------------------
 // FETCH FUNCTIONS
